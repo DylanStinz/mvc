f4f26285f8c8de59ee7b63b0dbb4e604df43b7b0
DylanStinz <23308060610028@cetis61.edu.mx>
2026-04-14 07:32:25 -0500

    pod

--- src/main.py
+++ src/main.py
@@ -4,25 +4,24 @@ from src.controllers.TareaController import TareaController
 from src.views.loginView import LoginView
 from src.views.dashboardView import DashboardView
 
-def main(page: ft.Page):
+def start(page: ft.Page):
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
-    
-    def route_change(route):
+    def route_change(e):
         page.views.clear()
-        if page.route == "/":
+        if page.route=="/":
+            page.add(ft.Text("Caso 1"))
             page.views.append(LoginView(page, auth_ctrl))
-        elif page.route == "/dashboard":
-            page.views.append(DashboardView(page, auth_ctrl, task_ctrl))
-            
+        elif page.route=="/dashboard":
+            page.views.append(DashboardView(page, task_ctrl))
         if not page.views:
             page.views.append(
-                ft.View("/", [ft.Text("error Ruta no encontrada")])
-            )
+                ft.View("/",[ft.Text("Error ruta no encontrada o vivista vacia")]))
+            
         page.update()
-        
     page.on_route_change = route_change
     page.go("/")
-    
+def main():
+    ft.app(target=start)
 if __name__ == "__main__":
-    ft.run(main)
+    main()    
