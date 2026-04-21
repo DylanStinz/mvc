37967975b02252236dbe876f78ec2fc5b5c7af34
DylanStinz <23308060610028@cetis61.edu.mx>
2026-04-20 21:29:51 -0600

    sas

--- src/views/loginView.py
+++ src/views/loginView.py
@@ -1,27 +1,47 @@
 import flet as ft
 def LoginView(page, auth_controller):
-    email_input= ft.Textfield(label = "Correo electrónico", width=350,border_radius=10)
-    pass_input= ft.TextField(label = "Contraseña", password=True, can_reveal_password=True, width=350,border_radius=10)
+    email_input= ft.TextField(
+        label = "Correo electrónico",
+        width=350,
+        border_radius=10,
+        keyboard_type=ft.KeyboardType.EMAIL
+    )
+    pass_input= ft.TextField(
+        label = "Contraseña", 
+        password=True,
+        can_reveal_password=True, 
+        width=350,
+        border_radius=10)
     
     def login_click(e):
-        user, msg= auth_controller.login(email_input.value, pass_input.value)
+        if not email_input.value or not pass_input.value:
+            page.snack_bar = ft.Snackbar(ft.Text("Por favor, complete todos los campos."))
+            page.snack_bar.open = True
+            page.update()
+            return
+        user, msg = auth_controller.login(email_input.value, pass_input.value)
+
         if user:
             page.session.set("user", user)
             page.go("/dashboard")
         else:
-            page.snack_bar = ft.Snackbar(ft.Text(msg))
+            page.snack_bar = ft.SnackBar(ft.Text(msg))
             page.snack_bar.open = True
             page.update()
             
-        return ft.View("/",[
-            ft.AppBar(title=ft.Text("SIGE - Login"), bgcolor=ft.colors.BLUE_GREY_900, color="white"),
-            ft.Column([
-        
-                ft.Text("Acceso al sistema", size=24, weight="bold"),
-                email_input,
-                pass_input,
-                ft.ElevatedButton("Iniciar sesión", on_click=login_click, width=350),
-                ft.TextButton("crear una cuenta nueva", on_click=lambda _: page.go("/register"))
+    return ft.View("/",
+            vertical_alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            appbar=ft.AppBar(title=ft.Text("Iniciar Sesión")),
             
-            
-        ],horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.MainAxisAlignment.CENTER)])
+            controls=[
+                ft.Column([
+                    ft.Text("Acceso al sistema", size=24, weight="bold"),
+                    email_input,
+                    pass_input,
+                    ft.TextButton("crear cuenta", on_click=lambda _: page.go("/register")),
+                ], horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    tight=True,
+                    spacing=20
+                ),
+            ])
